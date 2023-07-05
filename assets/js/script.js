085952321d7802371d78203043a7a0adda7ca1ef
zerin22 <tajrin.ini@gmail.com>
2023-07-05 18:44:19 +0600

    Record page complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,63 +20,63 @@ document.querySelector(".your_class").addEventListener("keypress", function (evt
 
 
 //for add new date and weight
-const formDataArray = [];
-const showDataElement = document.getElementById('showDateWeight'); 
-const myForm = document.getElementById('myForm'); 
+// const formDataArray = [];
+// const showDataElement = document.getElementById('showDateWeight'); 
+// const myForm = document.getElementById('myForm'); 
 
 
-function handleSubmit(event) {
-    event.preventDefault();
+// function handleSubmit(event) {
+//     event.preventDefault();
 
-    const dateValue = document.getElementById('date').value;
-    const weightValue = document.getElementById('weight').value;
+//     const dateValue = document.getElementById('date').value;
+//     const weightValue = document.getElementById('weight').value;
 
-    const formData = {
-        date: dateValue,
-        weight: weightValue
-    };
+//     const formData = {
+//         date: dateValue,
+//         weight: weightValue
+//     };
 
-    formDataArray.push(formData);
-    myForm.reset();
+//     formDataArray.push(formData);
+//     myForm.reset();
 
-    // localStorage.setItem('formDataArray', JSON.stringify(formDataArray));
+//     // localStorage.setItem('formDataArray', JSON.stringify(formDataArray));
 
-    console.log(formDataArray);
-    showDateWeight(formDataArray);
-}
+//     console.log(formDataArray);
+//     showDateWeight(formDataArray);
+// }
 
-function showDateWeight(formDataArray) {
-    formDataArray.forEach((formData) => {
-        const addCol = document.createElement('div');
-        addCol.classList.add('row');
-        // inject classname on addcol
-        addCol.innerHTML = `
-            <div class="col-sm-12 col-md-3 col-lg-3">
-                <div class="card mb-3 record__card" style="max-width: 18rem;">
-                    <div class="card-header bg-light"><h5>Person name</h5></div>
-                    <div class="card-body">
-                        <p class="card-text" id="showDate"><strong>Date: </strong>${formData.date}</p>
-                        <p class="card-text" id="showWeight"><strong>Weight: </strong>${formData.weight} kg</p>
-                    </div>
-                    <div class="card-footer bg-light">
-                        <div class="d-flex justify-content-between">
-                            <div>
-                                <button type="button" class="btn btn-primary" id="liveToastBtn"><i class="fa-regular fa-pen-to-square"></i></button>
-                                <button type="button" class="btn btn-secondary" id="liveToastBtn"><i class="fa-solid fa-cloud-arrow-down"></i></button>
-                            </div>
-                            <div>
-                                <button type="button" class="btn btn-danger" id="liveToastBtn"><i class="fa-solid fa-trash-can"></i></button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `;
-        showDataElement.appendChild(addCol); 
+// function showDateWeight(formDataArray) {
+//     formDataArray.forEach((formData) => {
+//         const addCol = document.createElement('div');
+//         addCol.classList.add('row');
+//         // inject classname on addcol
+//         addCol.innerHTML = `
+//             <div class="col-sm-12 col-md-3 col-lg-3">
+//                 <div class="card mb-3 record__card" style="max-width: 18rem;">
+//                     <div class="card-header bg-light"><h5>Person name</h5></div>
+//                     <div class="card-body">
+//                         <p class="card-text" id="showDate"><strong>Date: </strong>${formData.date}</p>
+//                         <p class="card-text" id="showWeight"><strong>Weight: </strong>${formData.weight} kg</p>
+//                     </div>
+//                     <div class="card-footer bg-light">
+//                         <div class="d-flex justify-content-between">
+//                             <div>
+//                                 <button type="button" class="btn btn-primary" id="liveToastBtn"><i class="fa-regular fa-pen-to-square"></i></button>
+//                                 <button type="button" class="btn btn-secondary" id="liveToastBtn"><i class="fa-solid fa-cloud-arrow-down"></i></button>
+//                             </div>
+//                             <div>
+//                                 <button type="button" class="btn btn-danger" id="liveToastBtn"><i class="fa-solid fa-trash-can"></i></button>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         `;
+//         showDataElement.appendChild(addCol); 
         
-        localStorage.setItem('addCol', JSON.stringify(addCol));
-    });
-}
+//         localStorage.setItem('addCol', JSON.stringify(addCol));
+//     });
+// }
 
-myForm.addEventListener('submit', handleSubmit);
+// myForm.addEventListener('submit', handleSubmit);
 
